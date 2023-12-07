b733896d1dad2ecc8e3bd3b836616220d472e5c6
takeru0076 <takeru200276@icloud.com>
2023-12-07 19:20:21 +0900

    Updated Fibonacci Calculation Method

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
-//BigInt 型の値を JSON 文字列に変換するため
-//JSONbig.stringify() メソッドを呼び出すことができるようになる
 const JSONbig = require('json-bigint');
 
-function calculateFibonacci(n) {
-  let current = BigInt(1);
-  let next = BigInt(1);
-  let tmp;
-  for (let i = 1; i < n; i++) {
-    tmp = current + next;
-    current = next;
-    next = tmp;
+function multiply(a, b) {
+  const c = initializeMatrix(a.length, b[0].length);
+  for (let i = 0; i < a.length; i++) {
+    for (let k = 0; k < b.length; k++) {
+      for (let j = 0; j < b[0].length; j++) {
+        c[i][j] += a[i][k] * b[k][j];
+      }
+    }
   }
-  return current;
+  return c;
+}
+
+function power(a, n) {
+  let result = initializeMatrix(a.length, a.length);
+  for (let i = 0; i < a.length; i++) {
+    result[i][i] = BigInt(1);
+  }
+  while (n > 0) {
+    if (n % 2 === 1) {
+      result = multiply(a, result);
+    }
+    a = multiply(a, a);
+    n >>= 1;
+  }
+  return result;
+}
+
+function initializeMatrix(xlen, ylen) {
+  const matrix = new Array(xlen);
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i] = new Array(ylen).fill(BigInt(0));
+  }
+  return matrix;
+}
+
+function fibonacci(n) {
+  const baseMatrix = [
+    [BigInt(1), BigInt(1)],
+    [BigInt(1), BigInt(0)],
+  ];
+  const poweredMatrix = power(baseMatrix, n + 1);
+  return poweredMatrix[1][0].toString();
 }
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-
 
 app.get('/fib', (req, res) => {
   try {
@@ -34,18 +63,17 @@ app.get('/fib', (req, res) => {
       throw new Error('Bad Request. Input must be a positive integer.');
     }
 
-    const data = calculateFibonacci(n);
-    //data オブジェクトを JSON 文字列に変換
-    const result = JSONbig.stringify({ result: data });
-    res.header('Content-Type', 'application/json')
-    res.send(result);
+    const result = fibonacci(n);
+    const response = JSONbig.stringify({ result });
+    res.header('Content-Type', 'application/json');
+    res.send(response);
   } catch (error) {
     res.status(400).json({ status: 400, message: error.message });
   }
 });
 
 app.use((req, res, next) => {
-  res.status(404).json({ status: 404, message: "Not found" });
+  res.status(404).json({ status: 404, message: 'Not found' });
 });
 
 app.listen(port, () => {
